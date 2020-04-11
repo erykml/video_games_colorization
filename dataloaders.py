Scaling by 255 only applied to tanh variant + bugfix

dataloaders.py
@@ -20,14 +20,13 @@ class ColorizationImageFolder(datasets.ImageFolder):
             img_original = self.transform(img)
             img_original = np.asarray(img_original)
             
-            # convert to lab
-            img_lab = rgb2lab(img_original / 255.0)
-            
             if self.lab_version == 1:
                 # output is in range [1,1] -> tanh activation
+                img_lab = rgb2lab(img_original / 255.0)
                 img_lab = (img_lab + [0, 0, 0]) / [100, 128, 128] 
             elif self.lab_version == 2:
                 # output is in range [0,1]
+                img_lab = rgb2lab(img_original)
                 img_lab = (img_lab + [0, 128, 128]) / [100, 255, 255]
             else:
                 raise ValueError('Incorrect Lab version!!!')

dl_utils.py
@@ -102,7 +102,7 @@ def show_model_results(model, model_name, lab_version, path, img_size, device):
     fig.show()
     
 class Upsample(nn.Module):
-    def __init__(self, scale_factor, mode):
+    def __init__(self, scale_factor=2, mode='nearest'):
         super(Upsample, self).__init__()
         self.interp = nn.functional.interpolate
         self.scale_factor = scale_factor